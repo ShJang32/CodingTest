df13ba85d1b6ecde46d345c48dcfb64fa91a84f1
ShJang32 <jangseohie32@gmail.com>
2024-09-30 15:09:24 +0900

    [Bronze III] Title: 최소, 최대, Time: 328 ms, Memory: 153832 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,13 +1,4 @@
 n = int(input())
 arr = list(map(int, input().split()))
-minimum = 1000000
-maximum = -1000000
 
-for element in arr:
-    if element < minimum:
-        minimum = element
-        
-    if element > maximum:
-        maximum = element
-
-print(minimum, maximum)
+print(min(arr), max(arr))
