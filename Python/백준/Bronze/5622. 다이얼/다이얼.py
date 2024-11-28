eed2ecb4cc2d3c50200895289c793196fdae0220
ShJang32 <jangseohie32@gmail.com>
2024-11-28 09:22:27 +0900

    [Bronze II] Title: 다이얼, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,31 @@
+word = input() # 2 ~ 15
+time = 0
+
+for alphabet in word:
+    if 65 <= ord(alphabet) <= 67:
+        time += 3
+
+    elif ord(alphabet) <= 70:
+        time += 4
+
+    elif ord(alphabet) <= 73:
+        time += 5
+
+    elif ord(alphabet) <= 76:
+        time += 6
+
+    elif ord(alphabet) <= 79:
+        time += 7
+
+    elif ord(alphabet) <= 83:
+        time += 8
+
+    elif ord(alphabet) <= 86:
+        time += 9
+
+    elif ord(alphabet) <= 90:
+        time += 10
+
+    else:
+        time += 1
+print(time)
